1b8ed89e81a9fcf29b8736cd308a39e46c391fba
Sara <saragorleta@yahoo.com>
2020-12-15 18:19:53 +0100

    add js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,55 @@
-document.getElementById("Genera").addEventListener('click',
+document.getElementById("genera").addEventListener('click',
 function() {
 
 // definizioni delle variabili
   var prezzoFinale;
-  var nome= document.getElementById('nome')value;
-  var eta= document.getElementById('eta')value;
-  var totKm= document.getElementById('km')value;
+  var nome= document.getElementById('nome').value;
+  var eta= document.getElementById('eta').value;
+  var totKm= document.getElementById('km').value;
 
   var prezzoBigliettoPieno= 0.21 * totKm;
   var tipoOfferta='prezzo standard';
 
-
-  if (eta<18){
+// definizioni delle condizioni
+  if (eta=='minorenne'){
     prezzoFinale=prezzoBigliettoPieno - (prezzoBigliettoPieno * 30/100);
     tipoOfferta='Sconto minorenne';
   }
-  else if(eta>65){
+  else if(eta=='over65'){
     prezzoFinale=prezzoBigliettoPieno - (prezzoBigliettoPieno * 40/100);
     tipoOfferta='Sconto Silver';
   }
   else{
     prezzoFinale=prezzoBigliettoPieno;
-
+    tipoOfferta='prezzo standard';
+  }
 
 // stampiamo i dati sul browser
-    document.getElementById('nome-passeggero').innerHTML=nome;
-    document.getElementById('Offerta-applicata').innerHTML=tipoOfferta;
-    document.getElementById('n-carrozza').innerHTML= Math.floor(Math.random()*9) +1;
-    document.getElementById('codice-cp').innerHTML=;
-    document.getElementById('costo-biglietto').innerHTML=prezzo finale.toFixed(2)+ ' '+ 'Euro';
+  document.getElementById('nome-passeggero').innerHTML=' '+nome;
+  document.getElementById('Offerta-applicata').innerHTML=tipoOfferta;
+  document.getElementById('n-carrozza').innerHTML= Math.floor(Math.random()*9) +1;
+  document.getElementById('codice-cp').innerHTML='codice';
+  document.getElementById('costo-biglietto').innerHTML=prezzoFinale.toFixed(2)+' Euro';
 
+  // per far apparire cliccando sul btn 'Genera'
+  document.getElementById('biglietto').className='show';
+}
+);
 
+document.getElementById("annulla").addEventListener('click',function() {
+  // andiamo ad annullare le variabili all'interno del form (nome km eta)
+  document.getElementById('nome').value='';
+  document.getElementById('eta').value='';
+  document.getElementById('km').value='';
+  // andiamo ad annullare i dati all'interno del biglietto
+  document.getElementById('nome-passeggero').innerHTML='';
+  document.getElementById('Offerta-applicata').innerHTML='';
+  document.getElementById('n-carrozza').innerHTML= '';
+  document.getElementById('codice-cp').innerHTML='';
+  document.getElementById('costo-biglietto').innerHTML='';
 
-
-
+  // per far scomparire cliccando sul btn 'annulla'
+  document.getElementById('biglietto').className='hidden';
 
 }
 );
